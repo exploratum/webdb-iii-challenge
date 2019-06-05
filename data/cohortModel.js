54b2eccb2fedc9b72381a9868c3f9c668f1a55e8
Thierry Joux <tjoux@hotmail.com>
2019-06-05 16:13:51 -0700

    Completed MVP

--- data/cohortModel.js
+++ data/cohortModel.js
@@ -7,7 +7,39 @@ function find() {
     return db('cohorts');
 }
 
+function findById(id) {
+    return db('cohorts')
+    .where({id})
+    .first();
+}
+
+function findStudentsByCohortId(cohort_id) {
+    return db('students')
+        .where({cohort_id});
+}
+
+function insert(cohort) {
+    return db('cohorts').insert(cohort);
+}
+
+function update(id, changes) {
+    return db('cohorts')
+    .where({id})
+    .update(changes)
+}
+
+function remove(id) {
+    return db('cohorts')
+    .where({id})
+    .del();
+}
+
 
 module.exports = {
-    find
+    find,
+    findById,
+    insert,
+    update,
+    remove,
+    findStudentsByCohortId
 };
